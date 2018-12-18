8bea875ee2d8d0c38378920812667f289e69807b
Barry Lu <alijk0803@126.com>
2018-12-19 01:16:38 +0800

    Basic Bone

---
+++ spider-sample/src/main/java/com/function/ProjectBoot.java
@@ -0,0 +1,11 @@
+package com.function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class ProjectBoot {
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBoot.class);
+	}
+}
